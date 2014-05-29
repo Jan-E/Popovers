c199ffe0ba4b73f0ac08a833d942addb7a52c914
Jay Versluis <versluis@Jays-MacBook-Pro.local>
2014-05-29 18:36:19 -0400

    Initial Commit

---
+++ Popovers/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Popovers
+//
+//  Created by Jay Versluis on 29/05/2014.
+//  Copyright (c) 2014 Pinkstone Pictures LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PopoversTests/PopoversTests.m
@@ -0,0 +1,34 @@
+//
+//  PopoversTests.m
+//  PopoversTests
+//
+//  Created by Jay Versluis on 29/05/2014.
+//  Copyright (c) 2014 Pinkstone Pictures LLC. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface PopoversTests : XCTestCase
+
+@end
+
+@implementation PopoversTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
