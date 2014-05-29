4c0ca22924c31191c6aadbfd46527c50ac504ac4
Jay Versluis <versluis@Jays-MacBook-Pro.local>
2014-05-29 19:17:08 -0400

    added Popover in Code

--- Popovers/ViewController.m
+++ Popovers/ViewController.m
@@ -7,8 +7,14 @@
 //
 
 #import "ViewController.h"
+#import "DateViewController.h"
 
-@interface ViewController ()
+@interface ViewController () <UIPopoverControllerDelegate>
+
+@property (strong, nonatomic) IBOutlet UIBarButtonItem *actionButton;
+@property (strong, nonatomic) IBOutlet UIDatePicker *mainDatePicker;
+@property (nonatomic, strong) UIPopoverController *popover;
+- (IBAction)actionButtonPressed:(id)sender;
 
 @end
 
@@ -24,6 +30,29 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)actionButtonPressed:(id)sender {
+    
+    // create a new Date Pikcer View Controller
+    UIStoryboard *storyboard = [UIStoryboard storyboardWithName:@"Main" bundle:nil];
+    DateViewController *dateView = [storyboard instantiateViewControllerWithIdentifier:@"DatePickerView"];
+    
+    // create a Popover Controller and present it
+    self.popover = [[UIPopoverController alloc]initWithContentViewController:dateView];
+    [self.popover presentPopoverFromBarButtonItem:self.actionButton permittedArrowDirections:UIPopoverArrowDirectionAny animated:YES];
+    self.popover.delegate = self;
+    
+    
+}
+
+#pragma mark - Popover Delegate
+
+- (void)popoverControllerDidDismissPopover:(UIPopoverController *)popoverController {
+    
+    // read out the date and populate our UI
+    DateViewController *dateView = (DateViewController *)popoverController.contentViewController;
+    self.mainDatePicker.date = dateView.datePicker.date;
 }
 
 @end
